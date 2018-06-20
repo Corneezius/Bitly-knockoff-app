08c84f744434765c2965a161e2220935a7942774
Corneezius <cfr2116@columbia.edu>
2018-06-20 02:42:31 -0400

    add client side meteor method

--- imports/collections/links.js
+++ imports/collections/links.js
@@ -1,3 +1,9 @@
 import { Mongo } from "meteor/mongo";
 
+Meteor.method({
+  "links.insert": function(url) {
+    console.log("attempting to save", url);
+  }
+})
+
 export const Links = new Mongo.Collection("links");
