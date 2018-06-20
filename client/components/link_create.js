f9a387b8e48065bea59390e2931ee5ae5bdb545f
Corneezius <cfr2116@columbia.edu>
2018-06-20 03:21:33 -0400

    add message prompt if url fails validation

--- client/components/link_create.js
+++ client/components/link_create.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
 
 class LinkCreate extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { error: ""};
+  }
+
+
+
+
+
   handleSubmit(event) {
     event.preventDefault();
     Meteor.call("links.insert", this.refs.link.value, (error) => {
+      if (error) {
+        this.setState({ error: "Enter a valid URL"})
+      }
+      else {
+        this.setState({error:""});
+      }
       console.log(error);
     });
   }
@@ -15,6 +31,7 @@ class LinkCreate extends Component {
         <label>Link to shorten</label>
         <input ref="link" className="form-control" />
       </div>
+      <div className="text-danger"> {this.state.error}</div>
       <button className="btn btn-primary">Shorten!</button>
     </form>
     );
